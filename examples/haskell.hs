48d0763f8e75ec6f9f3f83de175710f2520d50c1
Alex Veltman <alexveltman@protonmail.com>
2020-04-05 11:26:52 +0200

    Added to Haskell test

--- examples/haskell.hs
+++ examples/haskell.hs
@@ -4,4 +4,12 @@ a <$> b
 a <$ b
 a $> b
 a <+> b
-a <*> b 
+a <*> b
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    quicksort [y | y <- xs, y <= x] ++
+    [x] ++
+    quicksort[y | y <- xs, y > x]
+
